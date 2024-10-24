393dc0abe3ed98aa8f0aa55037d9a380a0dbbcf5
Marcos <marcosviniciosgoes1@gmail.com>
2024-10-24 10:10:54 -0300

    update

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1640161704729-cbe966a08476?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/marcosvgoes.png"/>
+
+                <strong>Marcos Goes</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
